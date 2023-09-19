ecffbd13dc78ad3c6e0293bf5d0c95e4ee6c3e3b
EvonDu <298550274@qq.com>
2023-09-19 15:06:57 +0800

    asd

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 测试参数
-configs=${2} #"hichip_hc15xx_cb_b100_v30_lock,n,n;hichip_hc15xx_cb_b200_v21_xbmp5_c1,y,y"
+configs=${1} #"hichip_hc15xx_cb_b100_v30_lock,n,n;hichip_hc15xx_cb_b200_v21_xbmp5_c1,y,y"
 echo ${configs}
 
 # 遍历配置文件
